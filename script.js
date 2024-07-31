c1ca8ca0d7ed8684bab25d5bec6ad899e68e371e
krishs05 <2222999@brunel.ac.uk>
2024-07-31 18:38:19 +0530

    vercel update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+window.addEventListener('scroll', () => {
+    const layers = document.querySelectorAll('.layer');
+    layers.forEach(layer => {
+        const speed = layer.getAttribute('data-speed');
+        const yPos = -(window.pageYOffset * speed);
+        layer.style.transform = `translateY(${yPos}px)`;
     });
 });
